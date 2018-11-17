51a20b2f49ea6c804b9236c1375cd195a304452c
natedunn2230 <dunn.113@wright.edu>
2018-11-16 21:17:13 -0500

    Initial commit

---
+++ n_dunn_project_01.py
@@ -0,0 +1,173 @@
+##############################################################################
+# Nathan Dunn
+# CS 3180
+# Project 1
+# RUSH lang 
+##############################################################################
+
+import sys
+sys.path.insert(0,"../..")
+
+# Python Version differences
+if sys.version_info[0] >= 3:
+    raw_input = input
+
+tokens = ('NUM', 'NAME', 'IF', 'ELSE', 'DO', 'WHILE', 'ASSIGN')
+literals = ['+', '*', '(', ')', '-', '/']
+
+t_NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_DO = r'$DO'
+t_WHILE = r'$WHILE'
+t_IF = r'$IF'
+t_ELSE = r'$ELSE'
+t_ASSIGN = r':='
+t_ignore = " \t\r"
+
+def t_NUM(t):
+    r'[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?'
+    t.value = float(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+import ply.lex as lex
+lex.lex()
+
+########################################################################################
+# BNF FOR RUSH
+# <expr> ::- <term> '+' <expr>
+#        | <term> '-' <expr>
+#        |   <term>
+#        | NAME ASSIGN <expr>
+#
+# <term> ::- <factor> '*' <term>
+#        |   <factor> '/' <term>
+#        |   <factor>
+#
+# <factor> ::- '(' <expr> ')'
+#        | NUM
+#        | NAME
+########################################################################################
+class Node:
+    symbols = { }
+    
+    def __init__(self):
+        self.children = []
+        self.text = "invalid"
+        self.function = lambda node: node.text
+
+    def interp(self):
+        """ Interprets the parse tree rooted at self """
+        return self.function(self)
+############################ HELPERS ####################################################
+
+def set_var_value_helper(node):
+    Node.symbols[node.children[0]] = node.children[1].interp()
+    return Node.symbols[node.children[0]]
+
+def get_var_value_helper(node):
+    try:
+        return Node.symbols[node.text]
+    except LookupError:
+        print("Undefined name '%s'" % node.text)
+        return 0
+
+############################ PARSER FUNCTIONS ###########################################
+# EXPRESSION
+def p_expr_term(p):
+    'expr : term'
+    p[0] = p[1]
+
+def p_expr_add(p):
+    'expr : term "+" expr'
+    p[0] = Node()
+    p[0].text = "+"
+    p[0].children = [p[1], p[3]]
+    p[0].function = lambda node: node.children[0].interp() + node.children[1].interp()
+
+def p_expr_sub(p):
+    'expr : term "-" expr'
+    p[0] = Node()
+    p[0].text = "+"
+    p[0].children = [p[1], p[3]]
+    p[0].function = lambda node: node.children[0].interp() - node.children[1].interp()
+
+def p_expr_assign(p):
+    'expr : NAME ASSIGN expr'
+    p[0] = Node()
+    p[0].text = '='
+    p[0].children = [p[1],p[3]]
+    p[0].function = lambda node: set_var_value_helper(node)
+
+# TERM
+def p_term_multiply(p):
+    '''term : factor '*' term '''
+    p[0] = Node()
+    p[0].text = "*"
+    p[0].children = [p[1], p[3]]
+    p[0].function = lambda node: node.children[0].interp() * node.children[1].interp()
+
+def p_term_divide(p):
+    '''term : factor '/' term '''
+    p[0] = Node()
+    p[0].text = "/"
+    p[0].children = [p[1], p[3]]
+    p[0].function = lambda node: node.children[0].interp() / node.children[1].interp()
+
+def p_term_factor(p):
+    '''term : factor'''
+    p[0] = p[1]
+# FACTOR
+def p_factor_parens(p):
+    '''factor : "(" expr ")"'''
+    p[0] = p[2]
+
+def p_factor_number(p):
+    '''factor : NUM'''
+    p[0] = Node()
+    p[0].text = str(p[1])
+    tmp = p[1]
+    p[0].function = lambda node: tmp
+
+def p_factor_name(p):
+    '''factor : NAME'''
+    p[0] = Node()
+    p[0].text = p[1]
+    p[0].function = lambda node: get_var_value_helper(node)
+
+# ERROR
+def p_error(p):
+    if p:
+        print("Syntax error at '%s'" % p.value)
+    else:
+        print("Syntax error at EOF")
+
+##################################### DRIVER #############################################
+import ply.yacc as yacc
+yacc.yacc()
+
+if 1 < len(sys.argv):
+    with open(sys.argv[1], 'r') as myfile:
+        data=myfile.read()
+    print data
+    resultNode = yacc.parse(data+'\n') # parse returns None upon error
+    if None != resultNode:
+        print resultNode.interp()
+
+else:
+    while 1:
+        try:
+            s = raw_input('RUSH > ')
+        except EOFError:
+            break
+        if not s: continue
+        resultNode = yacc.parse(s+'\n') # parse returns None upon error
+        if None != resultNode:
+            print resultNode.interp()
